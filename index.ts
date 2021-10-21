92ece13625f221a4a1a815fddb75522261c3077d
KnorpelSenf <shtrog@gmail.com>
2021-10-21 12:42:12 +0200

    Implement support for URL inputs

--- index.ts
+++ index.ts
@@ -33,29 +33,20 @@ const main = async (): Promise<void> => {
       }
     )
     .parse(Deno.args)
-  const filename: string = parsed.args[0]
+  const uri: string = parsed.args[0]
 
-  if (filename === undefined) {
-    console.log(errorText('Filename not detected.'))
+  if (uri === undefined) {
+    console.log(errorText('No input file detected.'))
     Deno.exit(1)
   }
 
-  try {
-    Deno.readTextFileSync(filename)
-  } catch (error) {
-    if (error.name === 'NotFound') {
-      console.log(errorText(`File "${filename}" not found.`))
-      Deno.exit(1)
-    } else {
-      throw error
-    }
-  }
+  await validateUri(uri)
 
   const startTime = Date.now()
   console.log(infoText('Reading files and generating nodes...'))
 
   const fileProcess = await Deno.run({
-    cmd: ['deno', 'doc', filename, '--reload', '--json'],
+    cmd: ['deno', 'doc', uri, '--reload', '--json'],
     stdout: 'piped'
   })
   const textDecoder = new TextDecoder()
@@ -105,5 +96,48 @@ const main = async (): Promise<void> => {
 }
 
 if (import.meta.main) {
-  main()
+  await main()
+}
+
+async function validateUri(uri: string) {
+  let url: URL
+  try {
+    url = new URL(uri)
+  } catch (error) {
+    console.log(errorText(`Cannot parse '${uri}' to a valid URI!`))
+    Deno.exit(1)
+  }
+
+  if (url.protocol.startsWith('http')) {
+    // try to fetch file from URL
+    const controller = new AbortController()
+    const handle = setTimeout(() => controller.abort(), 10 * 1000) // 10 seconds timeout
+    try {
+      const res = await fetch(uri, {
+        method: 'HEAD',
+        signal: controller.signal
+      })
+      console.log(infoText('Entrypoint file check status:', res.status))
+      clearTimeout(handle)
+    } catch (e) {
+      if (controller.signal.aborted) {
+        console.log(errorText(`Download from '${uri}' timed out.`))
+        Deno.exit(1)
+      } else {
+        throw e
+      }
+    }
+  } else {
+    // try to read file from disk
+    try {
+      await Deno.readTextFile(uri)
+    } catch (e) {
+      if (e.name === 'NotFound') {
+        console.log(errorText(`File "${uri}" not found.`))
+        Deno.exit(1)
+      } else {
+        throw e
+      }
+    }
+  }
 }
